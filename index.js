b98e16b248b3182507c0f2a64cf7aa2ef4a24dc2
Kristýna Konečná <kristyna.konecna02@gmail.com>
2024-04-06 11:47:48 +0200

    upraveny pozdravy

--- index.js
+++ index.js
@@ -2,13 +2,29 @@ const goodbye = (name) => {
   return 'S pozdravem ' + name;
 };
 
+const formalGoodbye = (name) => {
+  return `S uctivou poklonou ${name}`
+};
+
+const rudeGoodbye = (name) => {
+  return `Čus, měj se ${name}`
+};
+
 const fillSubject = (subject) => {
   document.querySelector('.email__subject').textContent = subject;
 };
 
-const fillBody = (body, name) => {
+const fillBody = (body, name, goodbyeFunction) => {
+  const renderGoodbye = goodbyeFunction(name);
   const bodyElm = document.querySelector('.email__body');
   bodyElm.innerHTML += body;
   const closingElm = document.querySelector('.email__closing');
-  closingElm.textContent = goodbye(name);
+  closingElm.textContent = renderGoodbye;
 };
+
+fillSubject('Obchodní sdělení');
+fillBody(
+  'Kupte mycí prostředek, který vám vytře zrak, se slevou 50 %.',
+  'Jan Čistý',
+  rudeGoodbye
+);
